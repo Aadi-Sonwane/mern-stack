22a99b086d4a5e5ebf2ef0ecd84fc3992e50587d
Aditya Sonwane <aadus.info@gmail.com>
2023-06-19 13:35:44 +0000

    setup project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+function App() {
+  return (
+    <div className="App"iii>
+      <h1 class='text-red-500 text-5xl'>Hello How are u today! </h1>
+    </div>
+  );
+}
+
+export default App;
